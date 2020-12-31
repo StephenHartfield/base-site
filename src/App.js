4470463026366a78002eb9c1e85b17603ae9e5c3
Stephen Hartfield <stephenjhartfield@gmail.com>
2020-12-31 13:48:54 -0600

    Init

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { Col, Row } from 'react-bootstrap';
+
+const AppStyled = styled.div`
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+    height: 30px;
+`;
+
+export default function App(props) {
+
+    return (
+        <div>
+            <div style={{ width: '100%', height: '500px', backgroundColor: 'lightgray' }}></div>
+            <Row>
+                <Col md={1}></Col>
+                <Col md={5}>
+                    <div style={{width: '90%', height: '300px', backgroundColor: 'lightblue', margin: '0 auto'}}></div>
+                </Col>
+                <Col md={4}>
+                    <div style={{width: '90%', height: '300px', backgroundColor: 'red', margin: '0 auto'}}></div>
+                </Col>
+            </Row>
+        </div>  
+    )
+}
