12d79aee1017a0b2d0245ffe8738d7ff895fb533
MatShadow9 <razorteeth1223@gmail.com>
2021-01-19 11:22:50 -0500

    Padding for About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import '../App.css';
 
 const Background = styled.div`
     background-size: cover;
@@ -15,6 +16,10 @@ const Header = styled.h1`
     margin-bottom: 50px;
 `;
 
+const Paragraph = styled.p`
+    padding: 15px 500px 10px 20px;
+`;
+
 export default function About(props) {
 
     return (
@@ -23,10 +28,14 @@ export default function About(props) {
                 <Header>About Us</Header>
             </Background>
             <br /><br />
-            <p>Insert text to fill up a lot, and I mean a lot, of space. All I have to do is think of what to fill it up with. 
-                I could tell a story, but that might be too much. After all, there's no need to write a novel here.
-                If there's a way to increase the padding on this paragraph, in order to make it thinner horizontally, that'd be much appreciated.
-                I also think there was supposed to be more here, but at the very least this is a good start.</p>
+            <Paragraph className="AboutText">
+                <p>Insert text to fill up a lot, and I mean a lot, of space. All I have to do is think of what to fill it up with. 
+                    I could tell a story, but that might be too much. After all, there's no need to write a novel here.
+                    Also the padding is working now. It makes this paragraph thinner and to the left side of the page, as if 
+                    there was an image on the right side. I also think there was supposed to be more here, but at the very least this is a good start.</p>
+            </Paragraph>
+            
+            
         </div>
     )
 }
